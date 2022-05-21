8c3c0241934c8a88399b9860d15b045664ed777a
rynxiao <yuzhongzi91@sina.com>
2022-05-21 12:19:29 +0800

    feat: add jotai demo

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,10 +1,12 @@
 import { useCallback } from 'react';
 import { View, Text, Button, Image } from '@tarojs/components';
-import { Button as VantButton } from '@antmjs/vantui';
+import { Field } from '@antmjs/vantui';
+import { useAtom } from 'jotai';
 import { useEnv, useNavigationBar, useModal, useToast } from 'taro-hooks';
 import logo from './hook.png';
 
 import './index.scss';
+import { textAtom, uppercaseAtom } from '../../states/store';
 
 const Index = () => {
   const env = useEnv();
@@ -17,6 +19,8 @@ const Index = () => {
     mask: true,
   });
   const [showToast] = useToast({ mask: true });
+  const [text, setText] = useAtom(textAtom);
+  const [uppercase] = useAtom(uppercaseAtom);
 
   const handleModal = useCallback(() => {
     show({ content: '不如给一个star⭐️!' }).then(() => {
@@ -24,9 +28,12 @@ const Index = () => {
     });
   }, [show, showToast]);
 
+  const onChange = (event) => {
+    setText(event.detail);
+  };
+
   return (
     <View className="wrapper">
-      <VantButton type="primary">Test</VantButton>
       <Image className="logo" src={logo} />
       <Text className="title">为Taro而设计的Hooks Library</Text>
       <Text className="desc">目前覆盖70%官方API. 抹平部分API在H5端短板. 提供近40+Hooks! 并结合ahook适配Taro!</Text>
@@ -34,6 +41,8 @@ const Index = () => {
         <Text className="label">运行环境</Text>
         <Text className="note">{env}</Text>
       </View>
+      <Text>{uppercase}</Text>
+      <Field value={text} placeholder="请输入用户名" onChange={onChange} />
       <Button className="button" onClick={() => setTitle('Taro Hooks Nice!')}>
         设置标题
       </Button>
